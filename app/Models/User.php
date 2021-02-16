be257dbe1f2c48ede59551cd367e709563b012a8
honorezemagho <honorezemagho@gmail.com>
2021-02-16 18:10:41 +0100

    fix model error

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,6 @@ class User extends Authenticatable
     ];
 
     public function role(){
-        return $this->belongsTo('App\Role');
+        return $this->belongsTo('App\Models\Role');
     }
 }
