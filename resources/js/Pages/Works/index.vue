194af94c9567e4e59f88d3f5246ebb7d8adafd64
Jordan Anafack <anafackjordan@gmail.com>
2021-05-21 23:43:27 +0100

    --- Auto-Git Commit ---

--- resources/js/Pages/Works/index.vue
+++ resources/js/Pages/Works/index.vue
@@ -1,22 +1,29 @@
 <template>
   <app-layout>
-     <InnerPageHero image-url="/dist/images/innerPageHeroAbout.jpg" title="About Us" />
-    
-      <div class="container mx-auto px-4 sm:px-0 py-8 sm:py-20">
-          <h1 class="text-3xl xl:text-4xl font-extrabold text-gray-800 mx-auto text-center  pb-4">Work Title</h1>   
+     <InnerPageHero image-url="/dist/images/banner-book-details.png" title="Work Research" />
+      <div class="text-2xl container mx-auto px-4 sm:px-0 py-8 sm:py-20">
+          Welcome to the My Digital Library Works collection! Some Works can be freely read and downloaded. Others can be borrowed and read in our online book reader.
+    </div>
+    <div>
+      <div class="container mx-auto grid sm:grid-cols-1 grid-cols-2 pt-6 gap-8">
+        <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+        <div class="rounded border-gray-300 dark:border-gray-700 border-dashed border-2 h-24"></div>
+     </div>
     </div>
   </app-layout>
 </template>
 
 <script>
 import InnerPageHero from "@/Components/InnerPageHero";
+import SideMenu from "@/Components/SideMenu";
 import AppLayout from "@/Layouts/AppLayout";
 
 
 export default {
   components: {
     InnerPageHero,
-    AppLayout
+    AppLayout,
+    SideMenu
   },
 
   props: {},
