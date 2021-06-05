cd73d81a696cc57308e853ad7f1170f9d8438cfb
Jordan Anafack <anafackjordan@gmail.com>
2021-06-05 09:14:20 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Mail;
+use App\Mail\MyTestMail;
 
 class SendMailController extends Controller
 {
     public function index()
     {
     	$user = User::find(1)->toArray();
-    	
-    	Mail::send('mail', $user, function($message) use ($user) {
-	        $message->to("anafackjordan@gmail.com");
-	        $message->subject('Welcome Mail');
-    	});
+        $details = [
+            'title' => 'Sample Title From Mail',
+            'body' => 'This is sample content we have added for this test mail'
+        ];
+
+        Mail::to('anafackjordan@gmail.com')->send(new MyTestMail($details));
 
     	dd('Mail Send Successfully');
     }
