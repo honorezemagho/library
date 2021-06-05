ea5fe82008ed3838dde97c64d0ebcf1fe5631a7d
Jordan Anafack <anafackjordan@gmail.com>
2021-06-05 14:57:10 +0100

    --- Auto-Git Commit ---

--- resources/js/Pages/WorkDetails/index.vue
+++ resources/js/Pages/WorkDetails/index.vue
@@ -86,10 +86,59 @@
       <!--Book details section -->
 
       <!--Book Items section -->
-       <div class="text-center w-full">
+       <div class="text-center w-full mb-5">
         <div class="text-center font-semibold text-gray-800 text-3xl">Book Items</div>
         <div class="w-32 mx-auto pt-3 border-b-2 animate-pulse  border-blue-800 text-center mb-4"></div>
           <p class="text-xl text-gray-600 mx-auto text-center">All the copies of this book in our Library</p>
+      </div>
+
+      <div class="container">
+         <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+        <thead class="text-white">
+          <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+            <th class="p-3 text-left">Name</th>
+            <th class="p-3 text-left">Email</th>
+            <th class="p-3 text-left" width="110px">Actions</th>
+          </tr>
+          <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+            <th class="p-3 text-left">Name</th>
+            <th class="p-3 text-left">Email</th>
+            <th class="p-3 text-left" width="110px">Actions</th>
+          </tr>
+                  <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th class="p-3 text-left">Name</th>
+                    <th class="p-3 text-left">Email</th>
+                    <th class="p-3 text-left" width="110px">Actions</th>
+                </tr>
+                  <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th class="p-3 text-left">Name</th>
+                    <th class="p-3 text-left">Email</th>
+                    <th class="p-3 text-left" width="110px">Actions</th>
+                </tr>
+        </thead>
+        <tbody class="flex-1 sm:flex-none">
+          <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+            <td class="border-grey-light border hover:bg-gray-100 p-3">John Covv</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">contato@johncovv.com</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+          </tr>
+          <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+            <td class="border-grey-light border hover:bg-gray-100 p-3">Michael Jackson</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">m_jackson@mail.com</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+          </tr>
+                  <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                      <td class="border-grey-light border hover:bg-gray-100 p-3">Julia</td>
+                      <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">julia@mail.com</td>
+                      <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                  </tr>
+                  <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">Martin Madrazo</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">martin.madrazo@mail.com</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                  </tr>
+        </tbody>
+		  </table>
       </div>
       <!--Book Items section -->
 
@@ -104,6 +153,25 @@
    </app-layout>
 </template>
 
+<style>
+ @media (min-width: 640px) {
+    table {
+      display: inline-table !important;
+    }
+
+    thead tr:not(:first-child) {
+      display: none;
+    }
+  }
+
+  td:not(:last-child) {
+    border-bottom: 0;
+  }
+
+  th:not(:last-child) {
+    border-bottom: 2px solid rgba(0, 0, 0, .1);
+  }
+</style>
 <script>
 import InnerPageHero from "@/Components/InnerPageHero";
 import Works from "@/Components/Works";
